600ed389f0ca1e019c07e7aed19843bb3f821dd7
Joshua Gonzales <joshuanicolaigonzales@gmail.com>
2024-06-18 17:47:35 +0800

    Room-1

--- Room.java
+++ Room.java
@@ -1,26 +1,48 @@
+/**
+ * This class is a representation of a Room object that has attributes such as: 
+ * the room "number", and the room "price". It also has getters and setters for
+ * each of its attributes.
+ */
 public class Room {
     
     private int number;
     private double price = 1299.00;
 
-    public Room(int number) {
-        this.number = number;
-        this.price = 1299.00;
+/**
+ * This is a constructor method that is used to create a "Room" instance.
+ */
+    public Room() {
+
     }
 
-    public Room(int number, int price) {
-        this.number = number;
-        this.price = price;
-    }
+/**
+ * This is a getter method to get the bumber of this "Room" instance.
+ * @return the number of this "Room" instance.
+ */
+public int getNumber() {
+    return this.number;
+}
 
+/**
+ * This is a getter method to get the price of this "Room" instance.
+ * @return the price of this "Room" instance.
+ */
     public double getPrice() {
         return this.price;
     }
 
-    public int getNumber() {
-        return this.number;
+/**
+ * This is a setter method to set the number os this "Room" instance.
+ * @param the number of this "Room" instance.
+ */
+    public void setNumber(int number) {
+        this.number = number;
     }
 
+/**
+ * This is a setter method to set the price os this "Room" instance.
+ * @param the price of this "Room" instance.
+ */
     public void setPrice(double price) {
         this.price = price;
     }
